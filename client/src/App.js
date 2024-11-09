3ad67577531c1ac03c5737636546c1b0a514ca46
Phouman <ryanphou@live.com>
2024-11-09 16:56:56 -0500

    i fixed stuff uwu

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+// client/src/App.js
+
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+import { Home } from './home.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/home.js
@@ -0,0 +1,12 @@
+import './home.css';
+import { Link } from 'react-router-dom';
+
+function Home(){
+    return (
+        <div className="home-body">
+            <p>HELLO WORLD RAWR XD</p>
+        </div>
+    )
+}
+
+export{Home};
