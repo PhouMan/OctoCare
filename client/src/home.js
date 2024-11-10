b70e29f579968cd88091432583f5141201d34ab6
Maya-Shamir <maya.shamir.99@gmail.com>
2024-11-10 09:26:43 -0500

    Landing page with Disclaimer Button

--- client/src/home.js
+++ client/src/home.js
@@ -3,13 +3,17 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as LockOff } from './images/LockOff.svg'
 import { ReactComponent as LockOn } from './images/LockOn.svg'
 import { ReactComponent as Octo } from './images/AltmanMascot1.svg'
+import { ReactComponent as Settings } from './images/SettingsSparkle.svg'
+import { ReactComponent as Disclaimer } from './images/Disclaimer.svg'
 
 function Home(){
     return (
         <div className="home-body">
-            <div className="landing-header">  
-                <Octo width="150" height="120"/> 
-                OctoCare
+            <div>
+                <div className="landing-header">  
+                    <Octo width="150" height="120"/> 
+                    OctoCare
+                </div>
             </div>
             {//emergency button will do things 
             }
@@ -39,8 +43,12 @@ function Home(){
                 <button className="locked">
                     <LockOn width="24" height="24"/>Section 8
                 </button>
-
+                <div className="footer">
+                    <Disclaimer width="26" height="26"/>
+                    Disclaimer: this DOES NOT count as a certification
+                </div>
             </div>
+            
         </div>
     )
 }
