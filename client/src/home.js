8ed19f9ba11b71c703f2a1b5c2b8f1650f1e1328
Phouman <ryanphou@live.com>
2024-11-09 20:57:25 -0500

    unfinished dang wants to see

--- client/src/home.js
+++ client/src/home.js
@@ -4,7 +4,21 @@ import { Link } from 'react-router-dom';
 function Home(){
     return (
         <div className="home-body">
-            <p>HELLO WORLD RAWR XD</p>
+            <div className="landing-header"></div>
+            {//emergency button will do things 
+            }
+            <div className="emergency">Emergency</div>
+            <div className="flex-container">
+                <div className="unlocked">Section 1</div>
+                <div className="locked">Section 2</div>
+                <div className="locked">Section 3</div>
+                <div className="locked">Section 4</div>
+                <div className="locked">Section 5</div>
+                <div className="locked">Section 6</div>
+                <div className="locked">Section 7</div>
+                <div className="locked">Section 8</div>
+
+            </div>
         </div>
     )
 }
