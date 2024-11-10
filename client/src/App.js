73677dfc83922c14a35e4e6342e3aea9fd331c49
nina2dev <nina2dev@gmail.com>
2024-11-10 12:40:46 -0500

    Add read page about stroke

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,9 @@ import React from "react";
 import logo from './logo.svg';
 import './App.css';
 import { Home } from './home.js';
-import { HomeCopy } from './home copy.js';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Section1 } from "./section1.js";
+import ReadPage from "./readPage.js";
 
 function App() {
   const [data, setData] = React.useState(null);
@@ -20,9 +20,16 @@ function App() {
   return (
     <div className="App">
         <Router>
+          
             <Routes>
+              
                 <Route path="/" element={<Home />} />
                 <Route path="/section1/" element={<Section1 />} />
+
+                <Route 
+                  path="/readSection1/:id" 
+                  element={<ReadPage file="section1.json" />} 
+                />
             </Routes>
         </Router>
     </div>
